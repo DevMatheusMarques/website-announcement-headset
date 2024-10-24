f8cdd9cfcfced4af6f8265c56d70db89bb7e01f3
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-10-24 04:40:48 -0300

    feat: NewTecnology and ImageCard components completed.

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "CardImage",
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card-new-tecnology">
+    <img :src="image" :alt="description" class="image">
+  </div>
+</template>
+
+<style scoped>
+.card-new-tecnology {
+  width: 100%;
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius: 100px;
+  overflow: hidden;
+}
+
+.image {
+  width: 100%;
+  height: 80%;
+  object-position: center;
+  margin-top: 5px;
+}
+
+</style>
